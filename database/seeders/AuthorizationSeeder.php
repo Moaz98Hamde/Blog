bedf5b23a31c1aec9f958fd9dd13005398ed6e4d
moaz hamde <moaz98hamde@gmail.com>
2022-11-13 02:01:51 +0200

    Declaring Authoriztion seeder & defining home routes for available roles

---
+++ database/seeders/AuthorizationSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+class AuthorizationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('roles')->truncate();
+        DB::table('users')->truncate();
+
+        Schema::enableForeignKeyConstraints();
+
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'blogger']);
+
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('secret')
+        ]);
+
+        $admin->assignRole('admin');
+
+    }
+}
