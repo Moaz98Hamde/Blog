6e9f87ce1b1136d3b6f21f32926fc6e3556c4306
moaz hamde <moaz98hamde@gmail.com>
2022-11-13 22:25:21 +0200

    Remove category model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,10 +21,6 @@ class Post extends Model
         'thumbnail',
     ];
 
-    public function category(){
-        return $this->belongsTo(Category::class);
-    }
-
     public function user(){
         return $this->belongsTo(User::class);
     }
